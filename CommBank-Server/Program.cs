310e4e1d949b429e31b5c014232df9040352abcd
Tien Bui <tienbui1508@gmail.com>
2024-02-16 03:27:26 +1030

    finish task 1

--- CommBank-Server/Program.cs
+++ CommBank-Server/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen();
 builder.Configuration.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("Secrets.json");
 
 var mongoClient = new MongoClient(builder.Configuration.GetConnectionString("CommBank"));
-var mongoDatabase = mongoClient.GetDatabase("CommBank");
+var mongoDatabase = mongoClient.GetDatabase("database");
 
 IAccountsService accountsService = new AccountsService(mongoDatabase);
 IAuthService authService = new AuthService(mongoDatabase);
