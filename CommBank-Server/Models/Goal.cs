58cf11036b8b1ad58522a4cb6a137ef774e71d9b
xyj140707 <xyj140707@gmail.com>
2024-02-26 10:17:34 -0500

    Support icons

--- CommBank-Server/Models/Goal.cs
+++ CommBank-Server/Models/Goal.cs
@@ -27,6 +27,4 @@ public class Goal
 
     [BsonRepresentation(BsonType.ObjectId)]
     public string? UserId { get; set; }
-
-    public string? Icon { get; set; }
 }
--- CommBank-Server/Program.cs
+++ CommBank-Server/Program.cs
@@ -42,7 +42,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
- 
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
