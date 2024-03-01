1db9bf3328fdb165ad853d53edf01f40ed7b8a3d
SiyaTM <siyatmahlangu@gmail.com>
2024-03-01 18:01:23 +0200

    Cover The GetGoalsForUser Route

--- CommBank.Tests/GoalControllerTests.cs
+++ CommBank.Tests/GoalControllerTests.cs
@@ -62,13 +62,32 @@ public class GoalControllerTests
         Assert.NotEqual(goals[1], result.Value);
     }
 
+    // Task 4: Cover Your Code!
+    // Cover The GetGoalsForUser Route.
     [Fact]
     public async void GetForUser()
     {
         // Arrange
-        
+        var goals = collections.GetGoals();
+        var users = collections.GetUsers();
+        IGoalsService goalsService = new FakeGoalsService(goals, goals[0]);
+        IUsersService usersService = new FakeUsersService(users, users[0]);
+        GoalController controller = new(goalsService, usersService);
+
         // Act
-        
+        var httpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext();
+        controller.ControllerContext.HttpContext = httpContext;
+        var result = await controller.GetForUser(goals[0].UserId!);
+
         // Assert
+        Assert.NotNull(result);
+
+        var index = 0;
+        foreach (Goal goal in result!)
+        {
+            Assert.IsAssignableFrom<Goal>(goal);
+            Assert.Equal(goals[0].UserId, goal.UserId);
+            index++;
+        }
     }
 }
